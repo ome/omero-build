04f0ba7e719049ea6b608ae0daf85b4dbf77186c
jmoore <josh@glencoesoftware.com>
2019-03-01 08:48:10 +0000

    Build docker in travis and bump version
    With this change, the state of the omero-build repo
    will be built as is in docker. The only change that
    is made is to update the versions in the submodules
    to point at one another (currently hard-coded as:
    `5.5.0-SNAPSHOT`). The built docker image is then
    passed to openmicroscopy's docker build so that the
    built 5.5.0-SNAPSHOT jars can be used without a
    rebuild.
    
    see: https://github.com/openmicroscopy/openmicroscopy/pull/5968

---
+++ travis.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+set -u
+set -x
+test -e build-infra || git clone git://github.com/ome/build-infra.git
+test -e openmicroscopy || git clone -b reuse-docker git://github.com/joshmoore/openmicroscopy.git
+test -e openmicroscopy/.omero || git clone -b srv-compose git://github.com/joshmoore/omero-test-infra.git openmicroscopy/.omero
+
+# build-infra/recursive-merge # Updates versions
+git submodule foreach "sed -iE 's/(omero-.*:5.5.0)-m3/\1-SNAPSHOT/' build.gradle"
+
+# For the moment: hard-coding these properties
+echo versions.omero-blitz=5.5.0-SNAPSHOT >> openmicroscopy/etc/omero.properties
+echo versions.omero-common-test=5.5.0-SNAPSHOT >> openmicroscopy/etc/omero.properties
+
+export BUILD_IMAGE=$(docker build -q -t omero-build . 2>/dev/null | awk '/Successfully built/{print $NF}')
+cd openmicroscopy
+.omero/docker srv
