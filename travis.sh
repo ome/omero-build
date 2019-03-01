de790b4d2c0adc34e0818a67d2ee510257423fe4
jmoore <josh@glencoesoftware.com>
2019-03-01 12:06:50 +0000

    Drop extended sed

--- travis.sh
+++ travis.sh
@@ -7,7 +7,7 @@ test -e openmicroscopy || git clone -b reuse-docker git://github.com/joshmoore/o
 test -e openmicroscopy/.omero || git clone -b srv-compose git://github.com/joshmoore/omero-test-infra.git openmicroscopy/.omero
 
 # build-infra/recursive-merge # Updates versions
-git submodule foreach "sed -iE 's/(omero-.*:5.5.0)-m3/\1-SNAPSHOT/' build.gradle"
+git submodule foreach "sed -ie 's/\(omero-.*:5.5.0\)-m3/\1-SNAPSHOT/' build.gradle"
 
 # For the moment: hard-coding these properties
 echo versions.omero-blitz=5.5.0-SNAPSHOT >> openmicroscopy/etc/omero.properties
