e0da576336dfc9b30c0c4e136245417beb377102
jmoore <josh@glencoesoftware.com>
2019-03-13 09:28:18 +0000

    Use new build-infra
     - all scripts are now in build-infra
     - no longer hard-coding "SNAPSHOT"
     - current state versions are propagated

--- travis.sh
+++ travis.sh
@@ -3,20 +3,26 @@ set -e
 set -u
 set -x
 
-printf "travis_fold:start:checkout\n"
-test -e openmicroscopy || git clone --depth=1 git://github.com/openmicroscopy/openmicroscopy.git
-test -e openmicroscopy/.build || git clone git://github.com/ome/build-infra.git openmicroscopy/.build
-test -e openmicroscopy/.omero || git clone git://github.com/openmicroscopy/omero-test-infra.git openmicroscopy/.omero
-printf "travis_fold:end:checkout\n"
+test -e .build || git clone git://github.com/ome/build-infra.git .build
+export PATH=$PATH:$PWD/.build
 
-openmicroscopy/.build/omero-image
+build-phase checkout
+test -e openmicroscopy || git clone --depth=1 git://github.com/openmicroscopy/openmicroscopy.git
+test -e openmicroscopy/.build || ln -s $PWD/.build openmicroscopy/.build
+test -e openmicroscopy/.omero || git clone git://github.com/openmicroscopy/omero-test-infra.git openmicroscopy/.omero
+
+build-phase setup
+foreach-set-dependencies
+foreach-get-version-as-property >> openmicroscopy/etc/omero.properties
+
+build-phase build_image
+docker build -t omero-build .
 export BUILD_IMAGE=$(docker inspect --format='{{ .Id }}' omero-build)
 echo $BUILD_IMAGE
 
-cd openmicroscopy/
-printf "travis_fold:start:test-infra\n"
+build-phase test_infra
+cd openmicroscopy
 export COMPOSE_FILE=srv-compose.yml
 .omero/compose up -d
 .omero/docker dev install
 .omero/docker dev wait_on_login
-printf "travis_fold:end:test-infra\n"
